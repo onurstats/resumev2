860b707d4ea1f1b0e3ca4c8489fcab9760770234
Onur Yanar <onur4185@gmail.com>
2020-10-21 11:01:59 +0300

    assets

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Onur Yanar Personal Site",
+    themeColor: "#212121",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/msapplication-icon-144x144.png"
+    },
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js"
+      // ...other Workbox options...
+    }
+  }
 };

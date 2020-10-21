bcbe5f0fcf91a160e6595cb7d37e73802720b2f3
Onur Yanar <onur4185@gmail.com>
2020-10-21 14:35:43 +0300

    rr

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -12,7 +12,7 @@ const execa = require("execa");
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
